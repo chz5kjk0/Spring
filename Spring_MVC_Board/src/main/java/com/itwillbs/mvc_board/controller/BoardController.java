c6c5cd4aec78f12adf133150a7048575bb84b269
chz5kjk0 <118323405+chz5kjk0@users.noreply.github.com>
2023-01-30 12:56:47 +0900

    230130
    - AJAX 및 JSON 활용

--- Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/controller/BoardController.java
+++ Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/controller/BoardController.java
@@ -12,12 +12,15 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -193,7 +196,7 @@ public class BoardController {
 //			@RequestParam(defaultValue = "") String keyword,
 //			@RequestParam(defaultValue = "1") int pageNum,
 //			Model model) {
-//		// ---------------------------------------------------------------------------
+		// ---------------------------------------------------------------------------
 //		// 페이징 처리를 위한 변수 선언
 //		int listLimit = 10; // 한 페이지에서 표시할 게시물 목록을 10개로 제한
 //		int startRow = (pageNum - 1) * listLimit; // 조회 시작 행번호 계산
@@ -241,26 +244,71 @@ public class BoardController {
 //	}
 	
 	// ===============================================================================
+	// 기본 목록 뷰페이지로 이동하는 서블릿 처리
+	@GetMapping("/BoardList.bo")
+	public String list() {
+		return "board/qna_board_list";
+	}
+	
 	// AJAX 요청을 통한 글목록 조회
 	// => AJAX 요청에 대한 JSON 데이터로 응답
 	// => 현재 메서드에서 JSON 타입 응답 데이터를 바로 생성하여 출력하기 위해
 	//    @ResponseBody 어노테이션 필요
 	// => 이동할 페이지가 없으므로 리턴타입 void
-	@GetMapping("/BoardList.bo")
-	public void list(
+	// => 만약, 응답 데이터의 한글이 깨질 경우 다음과 같이 매핑 데이터에 정보 추가
+	//    @GetMapping(value = "/BoardListJson.bo", produces = "application/json; charset=utf-8")
+	@ResponseBody
+	@GetMapping("/BoardListJson.bo")
+	public void listJson(
 			@RequestParam(defaultValue = "") String searchType,
 			@RequestParam(defaultValue = "") String keyword,
 			@RequestParam(defaultValue = "1") int pageNum,
 			Model model,
 			HttpServletResponse response) {
 		
+		// 페이징 처리를 위한 변수 선언
+		int listLimit = 10; // 한 페이지에서 표시할 게시물 목록을 10개로 제한
+		int startRow = (pageNum - 1) * listLimit; // 조회 시작 행번호 계산
+		// Service 객체의 getBoardList() 메서드를 호출하여 게시물 목록 조회
+		// ---------------------------------------------------------------------------
+		// => 파라미터 : 검색타입, 검색어, 시작행번호, 목록갯수   
+		// => 리턴타입 : List<BoardVO>(boardList)
+		List<BoardVO> boardList = service.getBoardList(searchType, keyword, startRow, listLimit);
+		// ---------------------------------------------------------------------------
+		// 자바 데이터를 JSON 형식으로 변환하기
+		// => org.json 패키지의 JSONObject 클래스를 활용하여 JSON 객체 1개를 생성하고
+		//    JSONArray 클래스를 활용하여 JSONObject 객체 복수개에 대한 배열 생성
+		// 0. JSONObject 객체 복수개를 저장할 JSONArray 클래스 인스턴스 생성
+		JSONArray jsonArray = new JSONArray();
+		
+		// 1. List 객체 크기만큼 반복
+		for(BoardVO board : boardList) {
+			// 2. JSONObject 클래스 인스턴스 생성
+			// => 파라미터 : VO(Bean) 객체(멤버변수 및 Getter/Setter, 기본생성자 포함)
+			JSONObject jsonObject = new JSONObject(board);
+//			System.out.println(jsonObject);
+			
+			// 참고. 저장되어 있는 JSON 데이터를 꺼낼 수도 있다! - get() 메서드 활용
+//			System.out.println(jsonObject.get("board_pass"));
+			
+			// 3. JSONArray 객체의 put() 메서드를 호출하여 JSONObject 객체 추가
+			jsonArray.put(jsonObject);
+		}
+		
+//		System.out.println(jsonArray);
+		// => JSONObject 복수개가 배열 형태로 JSONArray 객체에 저장되어 있음
+		// 또한, JSONArray 객체에서 JSONObject 객체를 꺼낼 수도 있다!
+//		JSONObject jsonObject = (JSONObject)jsonArray.get(0); // 첫번째 배열에서 꺼내기
+		// => 이 때, 리턴타입이 Object 타입이므로 JSONObject 타입 형변환 필요
+//		System.out.println(jsonObject.get("board_pass"));
+		
 		try {
-			// 응답 데이터를 직접 생성하여 웹페이지에 출력
-			// response 객체의 setCharacterEncodinf() 메서드로 출력 데이터 인코딩 지정 후
+			// 생성된 JSON 객체를 활용하여 응답 데이터를 직접 생성 후 웹페이지에 출력
+			// response 객체의 setCharacterEncoding() 메서드로 출력 데이터 인코딩 지정 후
 			// response 객체의 getWriter() 메서드로 PrintWriter 객체를 리턴받아
 			// PrintWriter 객체의 print() 메서드를 호출하여 응답데이터 출력
 			response.setCharacterEncoding("UTF-8");
-			response.getWriter().print("name:hong");
+			response.getWriter().print(jsonArray); // toString() 생략됨
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- Spring_MVC_Board/src/main/webapp/WEB-INF/views/board/qna_board_list.jsp
+++ Spring_MVC_Board/src/main/webapp/WEB-INF/views/board/qna_board_list.jsp
@@ -65,19 +65,79 @@
 </style>
 <script src="${pageContext.request.contextPath }/resources/js/jquery-3.6.3.js"></script>
 <script type="text/javascript">
+	// AJAX 를 활용한 게시물 목록 표시에 사용될 페이지 번호값 미리 저장
+	let pageNum = 1;
+	
 	$(function() {
+		// 검색타입(searchType)과 검색어(keyword) 값 가져와서 변수에 저장
+		let searchType = $("#searchType").val();
+		let keyword = $("#keyword").val();
+// 		alert(searchType + ", " + keyword);
+		
+		// 게시물 목록 조회를 처음 수행하기 위해 load_list() 함수 호출
+		// => 검색타입과 검색어를 파라미터로 전달(검색하지 않을 경우에도 동일) 
+		load_list(searchType, keyword);
+		
+		// 무한스크롤 기능 구현
+		// window 객체에서 scroll 동작 시 기능 수행(이벤트 처리)을 위해 scroll() 함수 호출
+		$(window).scroll(function() {
+// 			$("#listForm").before("확인");
+			// 1. window 객체와 document 객체를 활용하여 스크롤 관련 값 가져오기
+			// => 스크롤바 현재 위치, 문서 표시되는 창의 높이, 문서 전체 높이
+			let scrollTop = $(window).scrollTop();
+			let windowHeight = $(window).height();
+			let documentHeight = $(document).height();
+			
+// 			console.log("scrollTop : " + scrollTop + ", windowHeight : " + windowHeight + ", documentHeight : " + documentHeight + "<br>");
+
+			// 2. 스크롤바 위치값 + 창 높이 + x 가 문서 전체 높이 이상이면
+			//    다음 페이지 게시물 목록 로딩하여 추가
+			// => 이 때, x 값은 마지막으로부터 여유 공간으로 둘 스크롤바 아래쪽 남은 공간(픽셀값)
+			//    (x 값을 1로 지정 시 스크롤바가 바닥에 닿을 때 다음 페이지 로딩)
+			if(scrollTop + windowHeight + 1 >= documentHeight) {
+				// 다음 페이지 로딩하기 위한 load_list() 함수 호출
+				// => 이 때, 페이지 번호를 1 증가시켜 다음 페이지 목록 로딩
+				pageNum++;
+				load_list(searchType, keyword);
+			}
+		});
+	});
+	
+	// 게시물 목록 조회를 AJAX + JSON 으로 처리할 load_list() 함수 정의
+	// => 검색타입과 검색어를 파라미터로 지정
+	function load_list(searchType, keyword) {
 		$.ajax({
 			type: "GET",
-			url: "BoardList.bo?pageNum=1",
+// 			url: "BoardListJson.bo?pageNum=" + pageNum,
+			url: "BoardListJson.bo?pageNum=" + pageNum + "&searchType=" + searchType + "&keyword=" + keyword,
 			dataType: "json"
 		})
-		.done(function(boardList) {
-			$("#listForm > table").append(boardList);
+		.done(function(boardList) { // 요청 성공 시
+// 			$("#listForm > table").append(boardList);
+			
+			// JSONArray 객체를 통해 배열 형태로 전달받은 JSON 데이터를
+			// 반복문을 통해 하나씩 접근하여 객체 꺼내기
+			for(let board of boardList) {
+				// 테이블에 표시할 JSON 데이터 출력문 생성
+				// => 출력할 데이터는 board.xxx 형식으로 접근
+				let result = "<tr height='100'>"
+							+ "<td>" + board.board_num + "</td>"
+							+ "<td id='subject'>" 
+								+ "<a href='BoardDetail.bo?board_num=" + board.board_num + "'>"
+								+ board.board_subject + "</a></td>"
+							+ "<td>" + board.board_name + "</td>"
+							+ "<td>" + board.board_date + "</td>"
+							+ "<td>" + board.board_readcount + "</td>"
+							+ "</tr>";
+				
+				// 지정된 위치(table 태그 내부)에 JSON 객체 출력문 추가
+				$("#listForm > table").append(result);
+			}
 		})
 		.fail(function() {
 			$("#listForm > table").append("<h3>요청 실패!</h3>");
 		});
-	});
+	}
 </script>
 </head>
 <body>
@@ -91,13 +151,13 @@
 		<section id="buttonArea">
 			<form action="BoardList.bo">
 				<!-- 검색 타입 추가 -->
-				<select name="searchType">
+				<select name="searchType" id="searchType">
 					<option value="subject" <c:if test="${param.searchType eq 'subject'}">selected</c:if>>제목</option>
 					<option value="content" <c:if test="${param.searchType eq 'content'}">selected</c:if>>내용</option>
 					<option value="subject_content" <c:if test="${param.searchType eq 'subject_content'}">selected</c:if>>제목&내용</option>
 					<option value="name" <c:if test="${param.searchType eq 'name'}">selected</c:if>>작성자</option>
 				</select>
-				<input type="text" name="keyword" value="${param.keyword }">
+				<input type="text" name="keyword" id="keyword" value="${param.keyword }">
 				<input type="submit" value="검색">
 				&nbsp;&nbsp;
 				<input type="button" value="글쓰기" onclick="location.href='BoardWriteForm.bo'" />
